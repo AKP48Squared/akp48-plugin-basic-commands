84505e9b0c63220271f12ff0629100bf0d374293
Austin Peterson <austin@akpwebdesign.com>
2016-05-05 16:16:44 -0500

    Add getInstanceId command

---
+++ commands/getinstanceid.js
@@ -0,0 +1,9 @@
+function GetInstanceID() {
+  this.names = ['getinstanceid', 'instanceid', 'instance', 'id'];
+}
+
+GetInstanceID.prototype.respond = function (ctx) {
+  return ctx.instanceId;
+};
+
+module.exports = GetInstanceID;
