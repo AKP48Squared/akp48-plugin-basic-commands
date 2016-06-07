20995fe717d185f833917aaba4ec1a41e4edd13b
Austin Peterson <austin@akpwebdesign.com>
2016-06-07 17:23:39 -0500

    Begin update to new Context and Plugin systems.

--- main.js
+++ main.js
@@ -2,10 +2,14 @@
 
 class BasicCommands extends global.AKP48.pluginTypes.MessageHandler {
   constructor(AKP48) {
-    super('BasicCommands', AKP48);
+    super(AKP48, 'BasicCommands');
+    this.commands = {};
+    this._data = {};
+  }
+
+  load() {
     this._data = require('./plugin.json');
     var self = this;
-    this.commands = {};
     require('./commands').then(function(res){
       self.commands = res;
     }, function(err){
@@ -14,27 +18,20 @@ class BasicCommands extends global.AKP48.pluginTypes.MessageHandler {
   }
 }
 
-BasicCommands.prototype.handleCommand = function (message, context, resolve) {
-  global.logger.silly(`${this._pluginName}: Received command.`);
+BasicCommands.prototype.handleCommand = function (context) {
+  global.logger.silly(`${this.name}: Received command.`);
 
-  // prepare text.
-  context.originalText = context.text;
-  var text = context.text.split(' ');
-  var command = text[0];
-  text.shift();
-
-  context.text = text.join(' ');
-  context.command = command;
+  var command = context.command();
 
   //for each command we have
   for (var cmd in this.commands) {
     if (this.commands.hasOwnProperty(cmd)) {
       //check to see if it's the command we're wanting
-      global.logger.stupid(`${this._pluginName}: Checking ${cmd} command for ${command}.`);
+      global.logger.stupid(`${this.name}: Checking ${cmd} command for ${command}.`);
 
       //if this command is the one we're trying to run
       if(this.commands[cmd].names.includes(command.toLowerCase())) {
-        global.logger.stupid(`${this._pluginName}: Found command for ${command}.`);
+        global.logger.stupid(`${this.name}: Found command for ${command}.`);
 
         //set name to first alias of command, for permissions check purposes.
         var name = this.commands[cmd].names[0];
@@ -43,14 +40,14 @@ BasicCommands.prototype.handleCommand = function (message, context, resolve) {
         if(this._data && this._data.commands && this._data.commands[name] &&
           this._data.commands[name].perms && this._data.commands[name].perms.length) {
           //and we don't have any at all, simply log and do nothing else
-          if(!context.permissions) {
-            global.logger.debug(`${this._pluginName}: Command ${command} requires permissions and none were found.`);
+          if(!context.permissions()) {
+            global.logger.debug(`${this.name}: Command ${command} requires permissions and none were found.`);
             continue;
           }
           //but if we have some permissions, loop through command perms and see if we have any of them.
           var block = true;
           for (var i = 0; i < this._data.commands[name].perms.length; i++) {
-            if(context.permissions.includes(this._data.commands[name].perms[i])) {
+            if(context.permissions().includes(this._data.commands[name].perms[i])) {
               block = false;
               break;
             }
@@ -58,20 +55,16 @@ BasicCommands.prototype.handleCommand = function (message, context, resolve) {
 
           // If we don't have any of the permissions, log and leave
           if(block) {
-            global.logger.debug(`${this._pluginName}: Command ${command} requires permissions and none were found.`);
+            global.logger.debug(`${this.name}: Command ${command} requires permissions and none were found.`);
             continue;
           }
         }
 
         // If we get here, we passed all checks, so we just run it.
-        resolve(this.commands[cmd].respond(context));
+        context.reply(this.commands[cmd].respond(context));
       }
     }
   }
-
-  context.text = context.originalText;
 };
 
 module.exports = BasicCommands;
-module.exports.type = 'MessageHandler';
-module.exports.pluginName = 'basic-commands';
