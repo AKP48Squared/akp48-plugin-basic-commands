eb4547f270e990b060fd7462249e1752547677f6
Austin Peterson <austin@akpwebdesign.com>
2016-06-10 23:31:47 -0500

    Change how reload sends its alert.

--- commands/reload.js
+++ commands/reload.js
@@ -2,9 +2,9 @@ function Reload() {
   this.names = ['reload', 'rl'];
 }
 
-Reload.prototype.respond = function () {
+Reload.prototype.respond = function (c) {
   global.AKP48.reload();
-  return 'Reloading...';
+  c.reply(c.cloneWith({isAlert: true, text: 'Reloading...'}));
 };
 
 module.exports = Reload;
