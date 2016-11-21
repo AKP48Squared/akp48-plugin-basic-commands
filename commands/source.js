e393e6d96d7ec09b6178c10d35a52ef5f0e290ee
Austin Peterson <austin@akpwebdesign.com>
2016-11-21 14:13:16 -0600

    Change how source command works.
    This introduces a new parameter for the source command, --plugin.
    Using the parameter, we can tailor the URL to individual plugins.
    Also, this commit applies the style "link" to the output link.

--- commands/source.js
+++ commands/source.js
@@ -2,8 +2,9 @@ function Source() {
   this.names = ['source', 'src', 'repo', 'link'];
 }
 
-Source.prototype.respond = function () {
-  return 'https://github.com/AKP48Squared/AKP48Squared/';
+Source.prototype.respond = function (ctx) {
+  var text = `https://github.com/AKP48Squared/${ctx.args().plugin || 'AKP48Squared'}/`;
+  return {style: 'link', text: text};
 };
 
 module.exports = Source;
